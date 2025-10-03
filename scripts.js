92b43d4c5192c51cdc54e0c0118155d5f9cff6ce
Joao Ricado 78 <78joaoricardo@gmail.com>
2025-10-03 09:06:46 -0300

    quarto commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 /* javascript não é Java. */
+/* javascript é uma linguagem de script, interpretada, fraca e dinamicamente tipada. */
+/* Algoritmo: Sequência de passos para resolver um problema. 
+    1- Saber quem é o botão
+    2- Saber quando o botão foi clicado
+    3- Saber quem é Bola colorida
+    4- Trocar a cor da Bola colorida
+    5- Saber quem é o copo
+    6- Trocar a imagem do copo
+
+
+    javascript = script para inserir no index.html
+    document = HTML
+    querySelector = selecionar um elemento, pegar, buscar.
+*/
+
+
+let circulo = document.querySelector(".circulo") /*vou no index html e pego a classe cirulo*/
+let imagem = document.querySelector(".copo") /*vou no index html e pego a classe copo*/
+
+
+function trocarAcor(cor){
+    circulo.style.background = cor /*mudar a cor do circulo*/
+}
+
+function trocarImagem(endereco){
+    imagem.src = endereco /*mudar a imagem do copo*/
+}
